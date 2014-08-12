3525795fbd811e7f5256dfe97523b5ccbd3e6932
adrienp <adrienp@stanford.edu>
2014-08-12 09:40:07 -0700

    configure to watch apnt gps status

--- src/modules/everything_is_awesome/everything_is_awesome.c
+++ src/modules/everything_is_awesome/everything_is_awesome.c
@@ -21,7 +21,7 @@
 __EXPORT int everything_is_awesome_main(int argc, char *argv[]);
 
 int everything_is_awesome_main(int argc, char *argv[]) {
-	printf("Watching APNT status\n");
+	printf("Watching APNT GPS status\n");
 
 	/* subscribe to sensor_combined topic */
 	int apnt_sub_fd = orb_subscribe(ORB_ID(apnt_gps_status));
@@ -41,7 +41,7 @@ int everything_is_awesome_main(int argc, char *argv[]) {
 		int poll_ret = poll(fds, 1, 5000);
 
 		if (poll_ret == 0) {
-			printf("[everything_is_awesome] no apnt status change in last 5 seconds\n");
+			printf("[everything_is_awesome] no apnt gps status change in last 5 seconds\n");
 		} else if (poll_ret < 0) {
 			if (error_counter < 10 || error_counter % 50 == 0) {
 				printf("[everything_is_awesome] this means big problems\n");
@@ -49,11 +49,11 @@ int everything_is_awesome_main(int argc, char *argv[]) {
 			error_counter++;
 		} else {
 			if (fds[0].revents & POLLIN) {
-				struct apnt_gps_status_s apnt_status;
-				orb_copy(ORB_ID(apnt_gps_status), apnt_sub_fd, &apnt_status);
-				printf("[everything_is_awesome] APNT Status: \n"
-						"\t%llu\n\t%u\t%u\t%u\n", apnt_status.timestamp,
-						apnt_status.prn[0], apnt_status.elevation[0], apnt_status.snr[0]);
+				struct apnt_gps_status_s apnt_gps_status;
+				orb_copy(ORB_ID(apnt_gps_status), apnt_sub_fd, &apnt_gps_status);
+				printf("[everything_is_awesome] APNT GPS Status: \n"
+						"\t%llu\n\t%u\t%u\t%u\n", apnt_gps_status.timestamp,
+						apnt_gps_status.prn[0], apnt_gps_status.elevation[0], apnt_gps_status.snr[0]);
 			}
 		}
 	}
