58c915eb4a816ccbb3b4e2a0a43df60544e84726
Adrien Perkins <adrienp@stanford.edu>
2015-03-16 16:48:38 -0700

    working on trying to figure out why not creating yaw commands correctly

--- src/modules/navigator/hunt.cpp
+++ src/modules/navigator/hunt.cpp
@@ -45,6 +45,8 @@ Hunt::Hunt(Navigator *navigator, const char *name) :
 	_end_rotation_angle(0),
 	_in_rotation(false),
 	_allow_rotation_end(false),
+	_total_rotation(0.0f),
+	_prev_angle(0.0f),
 
 /* time */
 	_temp_time(hrt_absolute_time()),
@@ -173,6 +175,12 @@ Hunt::on_active()
 
 	} else if (_hunt_state == HUNT_STATE_ROTATE) {
 
+		// calculate the angle which we have rotated through so far
+		_total_rotation += _wrap_2pi(_navigator->get_global_position()->yaw - _prev_angle);
+		_prev_angle = _navigator->get_global_position()->yaw;
+
+		printf("the total angle of rotation so far = %f\n", (double) _total_rotation);
+
 		/* check to see if rotation finished */
 		if (_allow_rotation_end && is_mission_item_reached()) {
 			mavlink_log_info(_navigator->get_mavlink_fd(), "[HUNT] rotation completed");
@@ -187,7 +195,19 @@ Hunt::on_active()
 			report_cmd_finished();
 		} else {
 			/* if current location is w/in 5 deg of desired location, allow a stop */
+			/*
 			if (abs(_navigator->get_global_position()->yaw - _end_rotation_angle) <= math::radians(5.0)) {
+				_allow_rotation_end = true;
+			} */
+
+			/*
+			if (abs(_mission_item.yaw - _end_rotation_angle) <= math::radians(5.0)) {
+				_allow_rotation_end = true;
+			}
+			*/
+
+			if (_total_rotation >= math::radians(360.0f)) {
+				printf("we have rotated through 360 degrees\n");
 				_allow_rotation_end = true;
 			}
 
@@ -289,36 +309,66 @@ Hunt::set_next_item()
 		_hunt_state = HUNT_STATE_ROTATE;
 
 		if (!_in_rotation) {
+
 			mavlink_log_info(_navigator->get_mavlink_fd(), "[HUNT] rotating");
 			_in_rotation = true;
 			_end_rotation_angle = _navigator->get_global_position()->yaw; // want to rotate 360 degrees
+			_total_rotation = 0.0f;
+			_allow_rotation_end = false;
+			_prev_angle = _end_rotation_angle;
+
+			printf("[HUNT] setting to be in rotation\n");
+			printf("setting end rotation angle to %f\n", (double) _end_rotation_angle);
 		}
 
-		_current_rotation_direction = _tracking_cmd.yaw_angle;
+		_current_rotation_direction = (int) _tracking_cmd.yaw_angle;
+
+		if (_current_rotation_direction == 0) {
+			_current_rotation_direction = 1;
+		}
 
 		/* we want to just sit in one spot */
 		_mission_item.lat = _navigator->get_global_position()->lat;
-		_mission_item.lat = _navigator->get_global_position()->lon;
+		_mission_item.lon = _navigator->get_global_position()->lon;
 
 		/* if ending rotation */
 		if (_allow_rotation_end) {
+			// just keep the current yaw command
 			_mission_item.yaw = _navigator->get_global_position()->yaw;
-			break;
+		} else {
+			float yaw_step = _param_yaw_increment.get();
+			if (yaw_step > 0) {
+				_mission_item.yaw = _navigator->get_global_position()->yaw + math::radians((float) _current_rotation_direction * yaw_step);
+			} else {
+				_mission_item.yaw = _navigator->get_global_position()->yaw + math::radians((float) _current_rotation_direction * 10.0f);
+			}
+			_mission_item.yaw = _wrap_pi(_mission_item.yaw);
+
+			printf("setting yaw sp to %f\n", (double) _mission_item.yaw);
 		}
 
-		float yaw_step = _param_yaw_increment.get();
-		if (yaw_step > 0) {
-			_mission_item.yaw = _navigator->get_global_position()->yaw + _current_rotation_direction * yaw_step;
-		} else {
-			_mission_item.yaw = _navigator->get_global_position()->yaw + _current_rotation_direction * 10.0f;
-		}
-		_mission_item.yaw = _wrap_pi(_mission_item.yaw);
+
 
 		// setting the altitude of the mission item for a rotate command (just want to use current altitude)
 		_mission_item.altitude_is_relative = false;
 		_mission_item.altitude = _navigator->get_global_position()->alt;
 
 		break;
+	}
+	case HUNT_CMD_FINISH: {
+		/* change the hunt state to off */
+		_hunt_state = HUNT_STATE_OFF;
+
+		set_waiting();
+
+		// report the new cmd id
+		report_cmd_id();
+
+		// status must have changed if at this point, so report it
+		report_status();
+
+		// straight up return from here...
+		return;
 	}
 	default:
 		break;
--- src/modules/navigator/hunt.h
+++ src/modules/navigator/hunt.h
@@ -149,7 +149,8 @@ private:
 	float	_end_rotation_angle;			/**< the angle by which time rotation time should stop (same as initial...) */
 	bool	_in_rotation;					/**< true if in a rotation already, false otherwise */
 	bool	_allow_rotation_end;			/**< if true will start checking to see if we have reached the mission item */
-
+	float	_total_rotation;				/**< the angle through which we have rotated */
+	float	_prev_angle;					/**< the prev angle for total rotation calculation */
 
 	hrt_abstime _temp_time;
 	hrt_abstime _test_time;
